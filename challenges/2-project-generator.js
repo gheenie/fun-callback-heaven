57cae5b80845fc90d3051cba7a9b0d8f69445e60
infoomics <sbx_94@hotmail.com>
2023-02-01 11:36:55 +0000

    added code for mkdir base directory

--- challenges/2-project-generator.js
+++ challenges/2-project-generator.js
@@ -1,5 +1,12 @@
 const fs = require('fs');
 
-function projectGenerator(projectName, cb) {}
+function projectGenerator(projectName, cb) {
+    let fd = fs.mkdir(projectName, { recursive: true }, cb);
+
+    /*fs.open('mynewfile2.txt', 'w', function (err, file) {
+        if (err) throw err;
+        console.log('Saved!');
+    });*/
+}
 
 module.exports = projectGenerator;
