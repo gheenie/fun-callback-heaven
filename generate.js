6f2ef9714410bf16e69fcefd74342c44aadd7e6c
infoomics <sbx_94@hotmail.com>
2023-02-01 15:07:10 +0000

    added code for interaction with cli arguments

--- generate.js
+++ generate.js
@@ -2,18 +2,25 @@
 // Tells terminal to use node to execute this file.
 
 const projectGenerator = require('./challenges/2-project-generator');
-// To send commands to CLI.
+// For sending commands to CLI; async version.
 const exec = require('child_process').exec;
 
-//const projectName = process.argv.slice(-1)[0];
 const projectName = '/home/gk/northcoders-de/fun-callback-heaven/generatedTest';
 
 projectGenerator(projectName, function() {});
 
-exec(`npm install --only=dev --prefix ${projectName}`, function() {});
+//exec(`npm install --only=dev --prefix ${projectName}`, function() {});
+
+const cliArgs = process.argv;
+const lengthCliArgs = cliArgs.length;
+
+if (lengthCliArgs > 2) {
+    console.log(cliArgs[lengthCliArgs - 2], cliArgs[lengthCliArgs - 1]);
+    // Add to github remote, ASYNCHRONOUSLY.
+}
 
 // npm install -g PATH_TO/fun-callback-heaven/
-// WITH THE -g AND THE / AT THE END
+// MUST HAVE -g AND ENDING / 
 
 // npm list -g to check
 
