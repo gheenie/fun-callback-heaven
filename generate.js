d535c61646b79bd595469ab5a2ddbdef92590d50
infoomics <sbx_94@hotmail.com>
2023-02-01 13:47:18 +0000

    added code to test in cli

--- generate.js
+++ generate.js
@@ -1,5 +1,9 @@
+#!/usr/bin/env node
+// Tells terminal to use node to execute this file.
+
 const projectGenerator = require('./challenges/2-project-generator');
 
-const projectName = process.argv.slice(-1)[0];
+//const projectName = process.argv.slice(-1)[0];
+const projectName = '/home/gk/northcoders-de/fun-callback-heaven/generatedTest';
 
 projectGenerator(projectName, function() {});
